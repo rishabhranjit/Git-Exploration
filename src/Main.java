21bedf9c0cf9e8b4508e71f2088f6cf4da7e16eb
127ABhatt <127abhatt@frhsd.com>
2025-09-08 10:43:10 -0400

    I added code to make sure that rishabh understands that i editted it becauase i responded to the print statment with a print statement of my own. Just letting rishabh know what up!

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hi, my name is Rishabh");
+
+        boolean respond = true;
+        String name = "Anay Bhatt";
+        if (respond) {
+            System.out.println("Hi, I'm " + name);
+        }
     }
 }
